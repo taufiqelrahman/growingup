a195c02c3c888baa01b93c2faccf145eb2df16ea
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-05-20 09:54:25 +0700

    added ci-cd

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:12.16-alpine
+
+# Create app directory
+WORKDIR /usr/src/gu
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json *yarn* ./
+
+# Install yarn from the local .tgz
+# RUN npm install
+RUN yarn
+
+# Bundle app source
+COPY . ./
+
+# Building app
+# RUN npm run build
+
+# Running the app production
+# CMD [ "npm", "run", "start" ]
+# Running the app development
+# CMD [ "npm", "run", "dev" ]
+ENTRYPOINT ["yarn", "run", "deploy"]
+# ENTRYPOINT ["npm", "run", "start"]
--- src/views/BlogOverview.js
+++ src/views/BlogOverview.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col } from 'shards-react';
+import { Container, Row } from 'shards-react';
 
 import PageTitle from './../components/common/PageTitle';
-import SmallStats from './../components/common/SmallStats';
-import UsersOverview from './../components/blog/UsersOverview';
-import UsersByDevice from './../components/blog/UsersByDevice';
-import NewDraft from './../components/blog/NewDraft';
-import Discussions from './../components/blog/Discussions';
-import TopReferrals from './../components/common/TopReferrals';
+// import SmallStats from './../components/common/SmallStats';
+// import UsersOverview from './../components/blog/UsersOverview';
+// import UsersByDevice from './../components/blog/UsersByDevice';
+// import NewDraft from './../components/blog/NewDraft';
+// import Discussions from './../components/blog/Discussions';
+// import TopReferrals from './../components/common/TopReferrals';
 
-const BlogOverview = ({ smallStats }) => (
+const BlogOverview = () => (
   <Container fluid className="main-content-container px-4">
     {/* Page Header */}
     <Row noGutters className="page-header py-4">
-      <PageTitle title="Blog Overview" subtitle="Dashboard" className="text-sm-left mb-3" />
+      <PageTitle title="Overview" subtitle="Dashboard" className="text-sm-left mb-3" />
     </Row>
 
-    {/* Small Stats Blocks */}
+    {/* 
     <Row>
       {smallStats.map((stats, idx) => (
         <Col className="col-lg mb-4" key={idx} {...stats.attrs}>
@@ -37,31 +37,26 @@ const BlogOverview = ({ smallStats }) => (
     </Row>
 
     <Row>
-      {/* Users Overview */}
       <Col lg="8" md="12" sm="12" className="mb-4">
         <UsersOverview />
       </Col>
 
-      {/* Users by Device */}
       <Col lg="4" md="6" sm="12" className="mb-4">
         <UsersByDevice />
       </Col>
 
-      {/* New Draft */}
       <Col lg="4" md="6" sm="12" className="mb-4">
         <NewDraft />
       </Col>
 
-      {/* Discussions */}
       <Col lg="5" md="12" sm="12" className="mb-4">
         <Discussions />
       </Col>
 
-      {/* Top Referrals */}
       <Col lg="3" md="12" sm="12" className="mb-4">
         <TopReferrals />
       </Col>
-    </Row>
+      </Row>*/}
   </Container>
 );
 
